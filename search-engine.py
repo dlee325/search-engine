8f72c9b3479872b7645e5308005ff6966107209e
dlee325 <dlee325@gmail.com>
2017-02-13 11:11:41 +0100

    Give search engine functionality to start from seed page and crawl to others.

--- search-engine.py
+++ search-engine.py
@@ -7,15 +7,23 @@ page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">') #example url
 
 def get_next_target(page):
-    start_link = page.find('<a href=')
-    if start_link == -1:
-        return None, 0
+	'''Retrieves the next URL and the next starting point.'''
+	start_link = page.find('<a href=')
+	if start_link == -1:
+		return None, 0
 	start_quote = page.find('"', start_link)
 	end_quote = page.find('"', start_quote + 1)
 	url = page[start_quote + 1:end_quote]
 	return url, end_quote
 
+def union(p, q):
+	'''Helper function to add all links found on page to tocrawl'''
+	for e in q:
+		if e not in p:
+			p.append(e)
+
 def get_all_links(page):
+	'''Returns all URLs from a webpage's HTML'''
 	links = []
 	while True:
 		url, endpos = get_next_target(page)
@@ -25,4 +33,16 @@ def get_all_links(page):
 		else:
 			break
 	return links
+
+def crawl_web(seed):
+	'''Outputs a list of URLs that can be reached by a defined seed page.'''	
+	tocrawl = [seed]
+	crawled = []
+	while tocrawl:
+		page = tocrawl.pop()
+		if page not in crawled:
+			union(tocrawl, get_all_links(get_page(page)))
+			crawled.append(page)
+	return crawled
+
 
