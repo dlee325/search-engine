189545f22567da00a74957e812609e579a91654f
dlee325 <dlee325@gmail.com>
2017-02-13 10:08:03 +0100

    Code returning error. Check and fix line 17 ('end' is not defined).

--- search-engine.py
+++ search-engine.py
@@ -13,5 +13,4 @@ page =('<div id="top_bin"><div id="top_content" class="width960">'
 start_link = page.find('<a href=')
 start_quote = page.find('"', start_link)
 end_quote = page.find('"', start_quote + 1)
-url = page[start_quote:end_quote]
-print url[start_quote + 1:end_quote]
+url = page[start_quote + 1:end_quote]
