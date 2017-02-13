e1aa942a674e167638feac9733a8b170de686cc2
dlee325 <dlee325@gmail.com>
2017-02-13 10:48:18 +0100

    Encapsulate code in a function. Account for cases where no URL in HTML.

--- search-engine.py
+++ search-engine.py
@@ -1,16 +1,17 @@
 # My first search engine.
 
-# Test to see if code for extracting a url out of HTML works.
-
-# More specifically, it is code that assigns to the variable url 
-# a string that is the value of the first URL that appears in a 
-# link tag in the string page.
+# Encapsulate code in a function.
+# Account for cases where no URL in HTML.
 
 
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">') #example url
 
-start_link = page.find('<a href=')
-start_quote = page.find('"', start_link)
-end_quote = page.find('"', start_quote + 1)
-url = page[start_quote + 1:end_quote]
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+	start_quote = page.find('"', start_link)
+	end_quote = page.find('"', start_quote + 1)
+	url = page[start_quote + 1:end_quote]
+	return url, end_quote
